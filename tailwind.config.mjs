79bbfa5146a4f9f3fe607ecec99c1b351841fe5b
Aarav Agarwal <aarav.knp.08@gmail.com>
2024-08-13 21:15:53 +0530

    updated tailwind

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
@@ -9,12 +9,11 @@ module.exports = {
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
       colors: {
         "custom-background": "#212131",
-      }
+      },
     },
   },
   plugins: [],
