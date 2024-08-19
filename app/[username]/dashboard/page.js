6e159cbaadbc19d282fac864db8ba09908c1ebc5
Aarav Agarwal <aarav.knp.08@gmail.com>
2024-08-19 17:40:49 +0530

    Update page.js

--- app/[username]/dashboard/page.js
+++ app/[username]/dashboard/page.js
@@ -55,7 +55,7 @@ const Dashboard = () => {
         transition: Bounce,
       });
     } catch (error) {
-      toast.error(error, {
+      toast.error("Username Already Exists", {
         position: "top-right",
         autoClose: 2000,
         hideProgressBar: false,
@@ -119,6 +119,7 @@ const Dashboard = () => {
                     ? "password"
                     : "text"
                 }
+                disabled={field === "email"}
                 name={field}
                 id={field}
                 className="block w-full p-2.5 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 text-xs focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
