5aef88f9fc69e390b34ca52f38c643e3eeaf6f75
Aarav Agarwal <aarav.knp.08@gmail.com>
2024-08-19 21:30:23 +0530

    Update route.js

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -28,7 +28,7 @@ export const Authoptions = NextAuth({
   secret: process.env.NEXTAUTH_SECRET,
   session: {
     strategy: "jwt",
-    maxAge: 0,
+    maxAge: 24 * 60 * 60,
   },
   cookies: {
     sessionToken: {
@@ -36,9 +36,9 @@ export const Authoptions = NextAuth({
       options: {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "lax", 
+        sameSite: "lax",
         path: "/",
-        maxAge: 0, 
+        maxAge: 24 * 60 * 60,
       },
     },
   },
